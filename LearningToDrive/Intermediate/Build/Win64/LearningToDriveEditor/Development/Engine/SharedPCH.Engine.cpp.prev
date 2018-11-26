42b292e9e910da994d5a66e4269981d973ad08b7
Thomas Simon <thomas.simon@btopenworld.com>
2018-11-26 17:39:51 +0000

    It Works

---
+++ LearningToDrive/Intermediate/Build/Win64/LearningToDriveEditor/Development/Engine/SharedPCH.Engine.cpp.prev
@@ -0,0 +1,1 @@
+#include "A:/Projects/Unreal Engine Projects/LearningToDrive/LearningToDrive/Intermediate/Build/Win64/LearningToDriveEditor/Development/Engine/SharedPCH.Engine.h"
--- LearningToDrive/Source/LearningToDrive/PlayerCar.cpp
+++ LearningToDrive/Source/LearningToDrive/PlayerCar.cpp
@@ -28,20 +28,20 @@ APlayerCar::APlayerCar()
 
 	check(Vehicle4W->WheelSetups.Num() == 4);
 
-	Vehicle4W->WheelSetups[0].WheelClass = FrontWheel;
-	Vehicle4W->WheelSetups[0].BoneName = FName("WheelFrontLeft");
+	//Vehicle4W->WheelSetups[0].WheelClass = FrontWheel;
+	Vehicle4W->WheelSetups[0].BoneName = FName("Wheel_Front_Left");
 	Vehicle4W->WheelSetups[0].AdditionalOffset = FVector(0.f, -12.f, 0.f);
 
-	Vehicle4W->WheelSetups[1].WheelClass = FrontWheel;
-	Vehicle4W->WheelSetups[1].BoneName = FName("WheelFrontRight");
+	//Vehicle4W->WheelSetups[1].WheelClass = FrontWheel;
+	Vehicle4W->WheelSetups[1].BoneName = FName("Wheel_Front_Right");
 	Vehicle4W->WheelSetups[1].AdditionalOffset = FVector(0.f, 12.f, 0.f);
 
-	Vehicle4W->WheelSetups[2].WheelClass = RearWheel;
-	Vehicle4W->WheelSetups[2].BoneName = FName("WheelRearLeft");
+	//Vehicle4W->WheelSetups[2].WheelClass = RearWheel;
+	Vehicle4W->WheelSetups[2].BoneName = FName("Wheel_Rear_Left");
 	Vehicle4W->WheelSetups[2].AdditionalOffset = FVector(0.f, -12.f, 0.f);
 
-	Vehicle4W->WheelSetups[3].WheelClass = RearWheel;
-	Vehicle4W->WheelSetups[3].BoneName = FName("WheelRearRight");
+	//Vehicle4W->WheelSetups[3].WheelClass = RearWheel;
+	Vehicle4W->WheelSetups[3].BoneName = FName("Wheel_Rear_Right");
 	Vehicle4W->WheelSetups[3].AdditionalOffset = FVector(0.f, 12.f, 0.f);
 
 }
