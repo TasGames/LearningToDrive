7c6a259f952b6a2a655a2b5a395f03a505b211ed
Thomas Simon <thomas.simon@btopenworld.com>
2018-11-28 22:35:02 +0000

    Spawn building class

---
+++ LearningToDrive/Intermediate/Build/Win64/UE4Editor/Inc/LearningToDrive/SpawnBuildings.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LEARNINGTODRIVE_SpawnBuildings_generated_h
+#error "SpawnBuildings.generated.h already included, missing '#pragma once' in SpawnBuildings.h"
+#endif
+#define LEARNINGTODRIVE_SpawnBuildings_generated_h
+
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_RPC_WRAPPERS
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASpawnBuildings(); \
+	friend struct Z_Construct_UClass_ASpawnBuildings_Statics; \
+public: \
+	DECLARE_CLASS(ASpawnBuildings, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LearningToDrive"), NO_API) \
+	DECLARE_SERIALIZER(ASpawnBuildings)
+
+
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesASpawnBuildings(); \
+	friend struct Z_Construct_UClass_ASpawnBuildings_Statics; \
+public: \
+	DECLARE_CLASS(ASpawnBuildings, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LearningToDrive"), NO_API) \
+	DECLARE_SERIALIZER(ASpawnBuildings)
+
+
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASpawnBuildings(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASpawnBuildings) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASpawnBuildings); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASpawnBuildings); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASpawnBuildings(ASpawnBuildings&&); \
+	NO_API ASpawnBuildings(const ASpawnBuildings&); \
+public:
+
+
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASpawnBuildings(ASpawnBuildings&&); \
+	NO_API ASpawnBuildings(const ASpawnBuildings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASpawnBuildings); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASpawnBuildings); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ASpawnBuildings)
+
+
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_PRIVATE_PROPERTY_OFFSET
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_9_PROLOG
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_PRIVATE_PROPERTY_OFFSET \
+	LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_RPC_WRAPPERS \
+	LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_INCLASS \
+	LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_PRIVATE_PROPERTY_OFFSET \
+	LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_INCLASS_NO_PURE_DECLS \
+	LearningToDrive_Source_LearningToDrive_SpawnBuildings_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LearningToDrive_Source_LearningToDrive_SpawnBuildings_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ LearningToDrive/Source/LearningToDrive/SpawnBuildings.cpp
@@ -0,0 +1,27 @@
+// Thomas Arthur Simon
+
+#include "SpawnBuildings.h"
+
+
+// Sets default values
+ASpawnBuildings::ASpawnBuildings()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASpawnBuildings::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpawnBuildings::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
